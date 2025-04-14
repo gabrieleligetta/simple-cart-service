925d4b65259243e364873492e378a15b64d0be8b
gligetta <g.ligetta@notomia.com>
2025-04-14 19:29:46 +0200

    feat: added DatabaseModule

---
+++ libs/database/database.module.ts
@@ -0,0 +1,32 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({})
+export class DatabaseModule {
+  static register(): DynamicModule {
+    return {
+      module: DatabaseModule,
+      imports: [
+        ConfigModule,
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService) => {
+            return {
+              type: 'postgres' as const,
+              host: configService.get<string>('POSTGRES_HOST'),
+              port: configService.get<number>('POSTGRES_PORT'),
+              username: configService.get<string>('POSTGRES_USER'),
+              password: configService.get<string>('POSTGRES_PASSWORD'),
+              database: configService.get<string>('POSTGRES_DB'),
+              autoLoadEntities: true,
+              synchronize: true,
+              logging: ['query', 'error'],
+            };
+          },
+        }),
+      ],
+    };
+  }
+}
